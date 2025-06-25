4a4354f2ce0c7dc42eb957e9f727de5c1113b2e9
yuta-nakajima-1300 <vivi.vivi40.1vivi@gmail.com>
2025-06-25 15:00:34 +0900

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -19,7 +19,7 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase services
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const storage = getStorage(app);
 export const storage = getStorage(app);
 
 // Google Auth Provider
